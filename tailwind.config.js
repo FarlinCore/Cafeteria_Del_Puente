d9007891be3066b23bb4dafc52a25d638afe5945
FarlinCore <farlindiazcore@gmail.com>
2026-04-18 22:53:13 -0400

    proyecto actualizado a Tailwind con las clases integradas en el proyecto y no desde el proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./index.html"],
+    content: ["./*.html", "./*.js"],
     darkMode: 'class',
     theme: {
         extend: {}
