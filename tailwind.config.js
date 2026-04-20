7608ae3b37a69d9f619a1f1dfc074d7856afd3bc
FarlinCore <farlindiazcore@gmail.com>
2026-04-19 20:01:44 -0400

    cambios en el menu , sobre nosotros y nuestra gente

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,26 @@
 module.exports = {
     content: ["./*.html", "./*.js"],
     darkMode: 'class',
+    safelist: [
+        // Fondo de página
+        'bg-[#2c1608]',
+        // Fondo de cartas
+        'bg-[#4a2010]',
+        // Fondo de pastilla de avatares
+        'bg-[#6b3020]',
+        // Naranja primario (borde, texto, botón)
+        'border-[#F47E24]',
+        'border-l-4',
+        'text-[#F47E24]',
+        'hover:bg-[#F47E24]',
+        'hover:text-[#F47E24]',
+        'hover:text-white',
+        'group-hover:text-[#F47E24]',
+        // Navbar marrón
+        'bg-[#55301c]',
+        'text-[#55301c]',
+        'hover:bg-[#F47E24]',
+    ],
     theme: {
         extend: {}
     },
